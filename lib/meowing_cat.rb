e9f52d3103b4cf06479339f4c4813b4fc686d2c1
Leann Kim <leannminakim@gmail.com>
2018-03-26 23:02:46 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  attr_accessor :name
+
+  def name= (name)
+    @name = name
+  end
+
+  def name (name)
+    @name
+  end
+  
+  puts "meow!"
+end
